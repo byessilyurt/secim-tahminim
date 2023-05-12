ec508b30d35642b4d1b1e8d21371704a40817c39
byessilyurt <y.yesilyurt14@gmail.com>
2023-05-12 15:34:22 +0200

    eslint fix

--- src/components/ImageToBase64.js
+++ src/components/ImageToBase64.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 export default function ImageToBase64({ imgSrc, alt, className }) {
   const [base64Src, setBase64Src] = useState(null);
 
-  useEffect(() => {
-    getBase64FromUrl(imgSrc)
-      .then((base64Data) => setBase64Src(base64Data))
-      .catch((err) => console.error(err));
-  }, [imgSrc, getBase64FromUrl]);
-
-  const getBase64FromUrl = async (url) => {
-    const data = await fetch(url);
-    const blob = await data.blob();
-    return await getBase64FromBlob(blob);
-  };
-
-  const getBase64FromBlob = async (blob) => {
+  const getBase64FromBlob = useCallback(async (blob) => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onloadend = () => resolve(reader.result);
       reader.onerror = reject;
       reader.readAsDataURL(blob);
     });
-  };
+  }, []);
+
+  const getBase64FromUrl = useCallback(
+    async (url) => {
+      const data = await fetch(url);
+      const blob = await data.blob();
+      return await getBase64FromBlob(blob);
+    },
+    [getBase64FromBlob]
+  );
+
+  useEffect(() => {
+    getBase64FromUrl(imgSrc)
+      .then((base64Data) => setBase64Src(base64Data))
+      .catch((err) => console.error(err));
+  }, [imgSrc, getBase64FromUrl]);
 
   return <img src={base64Src} alt={alt} className={className} />;
 }
